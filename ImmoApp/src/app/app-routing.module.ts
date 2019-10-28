de913e635d0a2dfc3c100d19f6d86798851f802c
Unknown <Duncan.legoff@gmail.com>
2019-10-28 21:44:05 +0100

    Shaping, working towards home page

--- ImmoApp/src/app/app-routing.module.ts
+++ ImmoApp/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { VideoHomeComponent } from 'src/app/video-home/video-home.component';
+import { HomeComponent } from 'src/app/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'videoHome', component: VideoHomeComponent },
+  { path: '', component: HomeComponent },
+  { path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ImmoApp/src/app/app.module.ts
+++ ImmoApp/src/app/app.module.ts
@@ -4,15 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { VideoHomeComponent } from './video-home/video-home.component';
+import { MatSidenavModule } from '@angular/material';
+import { MatListModule } from '@angular/material';
+import { MatIconModule } from '@angular/material';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoHomeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
